265cc24d027c617ce755c148029d3786895f20ff
leothesen <leothesen@gmail.com>
2022-09-16 15:17:31 +0200

    removed links from the footer 🚫

--- src/footer/CenteredFooter.tsx
+++ src/footer/CenteredFooter.tsx
@@ -6,7 +6,7 @@ import { FooterIconList } from './FooterIconList';
 type ICenteredFooterProps = {
   logo: ReactNode;
   iconList: ReactNode;
-  children: ReactNode;
+  children?: ReactNode;
 };
 
 const CenteredFooter = (props: ICenteredFooterProps) => (
@@ -15,7 +15,7 @@ const CenteredFooter = (props: ICenteredFooterProps) => (
 
     <nav>
       <ul className="navbar mt-5 flex flex-row justify-center font-medium text-xl text-gray-800">
-        {props.children}
+        {props.children && props.children}
       </ul>
     </nav>
 
