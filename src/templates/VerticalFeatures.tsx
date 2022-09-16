70b78ea9aa74c09bb8960879e3b1b297243ba6b1
leothesen <leothesen@gmail.com>
2022-09-16 15:15:56 +0200

    updated copy for feature list 📝

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,25 +3,38 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    // title="AltraMoney"
+    description="AltraMoney is a financial planning application that aims to sustainably help as many people as possible to have their money be directed intelligently to serve their values."
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Financial plan"
+      description="Build out a financial plan that aligns with your personal values using the AltraBlueprint."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Suggestions and action items"
+      description="Take action with a personalised list of actions to take ownership of your money."
       image="/assets/images/feature2.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Education"
+      description="Educational content"
+      image="/assets/images/feature3.svg"
+      imageAlt="Third feature alt text"
+    />
+    <VerticalFeatureRow
+      title="Advice"
+      description="Get financial advice from professionals and a community forum"
+      image="/assets/images/feature3.svg"
+      imageAlt="Third feature alt text"
+      reverse
+    />
+    <VerticalFeatureRow
+      title="Altruisum"
+      description="Do good in the world by donating money in an effectively alruistic way."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
